050077b08d032f8376eabace3cee0665fdf2f163
DESKTOP-LFOCKGF\David Noguera <davidnoguera0912@gmail.com>
2025-04-27 11:05:21 -0500

    Una Sola Pagina

--- medifinder-afiliacion/src/App.jsx
+++ medifinder-afiliacion/src/App.jsx
@@ -1,9 +1,10 @@
 import { FormProvider } from './contexts/FormContext'
 import FormLayout from './components/layout/FormLayout'
+import './index.css'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="fondo py-12 px-4 sm:px-6 lg:px-8">
       <FormProvider>
         <FormLayout />
       </FormProvider>
